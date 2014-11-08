0d34e735a518f10668f8aef419fcc283868e7fc6
Martin Schmid <m.schmid@equitania.com>
2014-11-08 15:58:06 +0100

    Copyrights ergänzt

--- scripts/restore-db-odoo-server.sh
+++ scripts/restore-db-odoo-server.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 # Mit diesem Skript wird ein Restore einer Odoo Datenbank durchgeführt
 # With this script you can restore a odoo db on postgresql
+##############################################################################
+#
+#    Shell Script for Odoo, Open Source Management Solution
+#    Copyright (C) 2014-now Equitania Software GmbH(<http://www.equitania.de>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 mybasepath="/opt/odoo"
 mybackuppath=$mybasepath"/backup"
