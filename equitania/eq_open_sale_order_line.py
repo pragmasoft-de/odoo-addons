d04a05817afc28c45c9d4580216668a5a4b347d7
Artur Bertram <a.bertram@equitania.de>
2015-01-12 16:19:26 +0100

    [ADD] Added the SQL Query for creating the table

--- equitania/eq_open_sale_order_line.py
+++ equitania/eq_open_sale_order_line.py
@@ -19,29 +19,43 @@
 #
 ##############################################################################
 
+from openerp import tools
 from openerp import models, fields, api, _
 from datetime import datetime, timedelta, date
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT as OE_DFORMAT
 
 class eq_open_sale_order_line(models.Model):
+    _auto = False
     _name = 'eq_open_sale_order_line'
-    
-    """ hier wird noch der rest implementiert """
     
     # Felder
     eq_order_id = fields.Many2one('sale.order', string="Sale Order")
+    eq_framework_agreement_id = fields.Many2one('eq_framework_agreement', string="Framework Agreement")
     eq_customer_no = fields.Char(size=64, string="Customer No")
     eq_customer = fields.Many2one('res.partner', string="Customer")    
     eq_delivery_date = fields.Date(string="Delivery date")
     eq_pos = fields.Integer(string="Seq")
     eq_quantity = fields.Integer(string="Quantity")
-    eq_quantity_left = fields.Integer(string="Quantity left")    
+    eq_quantity_left = fields.Integer(string="Quantity left")
     eq_product_no = fields.Many2one('product.product', string="Product number")    
     eq_description = fields.Text(string="Description")
     eq_drawing_no = fields.Char(size=100, string="Drawing number")
     
-    def generate_statement(self):
-        """ hier wird der Statement gespeichert und kann später geändert werden """
-        sql = "todo"
-        return sql
-        
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, 'eq_open_sale_order_line')
+        cr.execute("""
+            CREATE OR REPLACE VIEW eq_open_sale_order_line AS (
+            (SELECT MIN(id) as id,
+            order_id as eq_order_id,
+            eq_agreement_id as eq_framework_agreement_id,
+            (select eq_customer_ref from res_partner where id = (select partner_id from sale_order where id = order_id)) as eq_customer_no,
+            (select partner_id from sale_order where id = order_id) as eq_customer,
+            eq_delivery_date,
+            sequence as eq_pos,
+            product_uom_qty as eq_quantity,
+            (select SUM(product_qty) from stock_move where procurement_id = (select id from procurement_order where sale_line_id = main.id) and state != 'done') as eq_quantity_left,
+            product_id as eq_product_no,
+            (select description_sale from product_template where id = (select product_tmpl_id from product_product where id = product_id)) as eq_description,
+            (select eq_drawing_number from product_template where id = (select product_tmpl_id from product_product where id = product_id)) as eq_drawing_no
+            FROM sale_order_line as main
+            Group by eq_order_id, eq_agreement_id, eq_customer_no, eq_customer, eq_delivery_date, eq_pos, eq_quantity, eq_product_no, eq_description, eq_drawing_no, main.id))""")
