b92531fb19a0626c3990bd8b48f7f3d698363fd0
Michal Sodomka <m.sodomka@equitania.de>
2015-01-22 14:29:04 +0100

    [CHG] Deleted eq_inventorylist. It was created as a new module, so we don't need it in odoo-addons anymore

--- equitania/__init__.py
+++ equitania/__init__.py
@@ -29,6 +29,6 @@ import eq_sale_order_seq
 import eq_partner_extension
 import eq_report_extension
 import eq_lead_referred
-import eq_inventorylist
+#import eq_inventorylist
 import eq_open_sale_order_line
 import res_groups
