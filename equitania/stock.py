9acb3f84490e86a7f4ac1bfaa1e9d00c6b815e74
Artur Bertram <a.bertram@equitania.de>
2015-01-26 19:57:41 +0100

    [FIX] Direkten Verglich zwischen zwei double precision in einenen ungefähren Vergleich abgeändert.

--- equitania/stock.py
+++ equitania/stock.py
@@ -37,8 +37,8 @@ class stock_picking_extension(osv.osv):
         res['group_id'] = op.picking_id.group_id.id
         
         stock_move_obj = self.pool.get('stock.move')
-        same_move_id = stock_move_obj.search(cr, uid, [('price_unit', '=', res['price_unit']), ('product_id', '=', res['product_id']), ('picking_id', '=', res['picking_id'])])[0]
-        same_move = stock_move_obj.browse(cr, uid, same_move_id).procurement_id.id
+        same_move_id = stock_move_obj.search(cr, uid, [('price_unit', '>=', res['price_unit'] - 0.01), ('price_unit', '<=', res['price_unit'] + 0.01), ('product_id', '=', res['product_id']), ('picking_id', '=', res['picking_id'])])
+        same_move = stock_move_obj.browse(cr, uid, same_move_id)
         res['procurement_id'] = same_move.procurement_id.id
         res['name'] = same_move.name
         return res
