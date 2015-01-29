d9f837f1da6f7a2a5306da2b9509229b942bbcfc
Artur Bertram <a.bertram@equitania.de>
2015-01-29 20:14:01 +0100

    [CHG] Auftragsnummer wird in der Rechnugn im Feld name gespeichert.

--- equitania/eq_report_extension.py
+++ equitania/eq_report_extension.py
@@ -133,7 +133,6 @@ class eq_report_extension_sale_order(osv.osv):
         invoice_vals = {
             'name': order.name,
             'origin': order.name or False,
-            'eq_sale_order': order.name,
             'type': 'out_invoice',
             'reference': order.client_order_ref or order.name,
             'account_id': order.partner_id.property_account_receivable.id,
@@ -292,7 +291,6 @@ class eq_report_extension_invoice(osv.osv):
                 'eq_head_text': fields.text('Head Text'),
                 'eq_ref_number': fields.char('Sale Order Referenc', size=64),
                 'eq_delivery_address': fields.many2one('res.partner', 'Delivery Address'),
-                'eq_sale_order': fields.char('Sale Order', size=64),
                 }
     _defaults = {
                 'eq_contact_person_id': lambda obj, cr, uid, context: obj.pool.get('hr.employee').search(cr, uid, [('user_id', '=', uid)])[0] if len(obj.pool.get('hr.employee').search(cr, uid, [('user_id', '=', uid)])) >= 1 else obj.pool.get('hr.employee').search(cr, uid, [('user_id', '=', uid)]) or False 
@@ -328,7 +326,7 @@ class eq_report_extension_stock_picking(osv.osv):
     
     def _get_invoice_vals(self, cr, uid, key, inv_type, journal_id, move, context=None):
         res = super(eq_report_extension_stock_picking, self)._get_invoice_vals(cr, uid, key, inv_type, journal_id, move, context)
-        res['eq_sale_order'] = move.picking_id.origin
+        res['name'] = move.picking_id.origin
         return res
     
 class eq_compatibility_equitania_inox(osv.osv):
