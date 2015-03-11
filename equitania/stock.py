bd1bf6643ce033d69075db563766ca3c87a1e549
Artur Bertram <a.bertram@equitania.de>
2015-03-11 16:54:45 +0100

    [FIX] Underdelivery and overdelivery working normaly. Stock operation are deleted after creation of the underdelivery.

--- equitania/stock.py
+++ equitania/stock.py
@@ -67,7 +67,9 @@ class stock_picking_extension(osv.osv):
                     if pack_operation.product_qty > qty:
                         packop_qty = pack_operation.product_qty - qty + move_qty
                         self.pool.get('stock.move').write(cr, uid, move_id, {'product_uom_qty': packop_qty, 'product_uos_qty': packop_qty * pack_operation.product_id.uos_coeff})
-        return super(stock_picking_extension, self).do_transfer(cr, uid, picking_ids, context)
+        res = super(stock_picking_extension, self).do_transfer(cr, uid, picking_ids, context)
+        cr.execute('delete from stock_pack_operation')
+        return res
     
     @api.multi
     def reverse_picking_new_view(self):
