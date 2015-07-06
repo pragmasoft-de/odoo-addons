fbc48ac0cbc1ab00bc529b0fab51603f64e7cf13
Artur Bertram <a.bertram@equitania.de>
2015-07-06 15:30:37 +0200

    [CHG] context is a keyword

--- equitania/eq_foreign_ref.py
+++ equitania/eq_foreign_ref.py
@@ -44,7 +44,7 @@ class eq_purchase_order(osv.osv):
             @context: context
             @return: original values together with eq_foreign_ref stored in partner_ref
         """        
-        original_values = super(eq_purchase_order, self).onchange_partner_id(cr, uid, ids, partner_id, context)
+        original_values = super(eq_purchase_order, self).onchange_partner_id(cr, uid, ids, partner_id, context=context)
         if partner_id:
             partner = self.pool.get('res.partner')
             supplier = partner.browse(cr, uid, partner_id, context=context)            
