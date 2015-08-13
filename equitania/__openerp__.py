c15eefcf9dcca8e52769f4cbaa766cb1c9bd2b84
Artur Bertram <a.bertram@equitania.de>
2015-08-13 14:34:53 +0200

    [FIX] eq_ref_number and name of the invoice are set correctly for every advance payment method.

--- equitania/__openerp__.py
+++ equitania/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Equitania Erweiterungsmodul',
     'license': 'AGPL-3',
-    'version': '1.0.58',
+    'version': '1.0.60',
     'description': """
         Equitania Software GmbH
     """,
