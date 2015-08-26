ef4e8dc1b7faebd9f4b9999e744a05ed1e708c23
Martin Schmid <m.schmid@equitania.com>
2015-08-26 11:11:55 +0200

    [CHG] restore-zip.sh angepasst.

--- scripts/restore-zip.sh
+++ scripts/restore-zip.sh
@@ -21,7 +21,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-export PGPASSWORD=odoo2015
+export PGPASSWORD=
 mybackuppath="$PWD"
 mybasepath="$HOME"
 
