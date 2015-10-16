305ff200badd2815ee340bb687badd70efffed9e
Martin Schmid <m.schmid@equitania.com>
2015-10-16 16:14:47 +0200

    [CHG] Update Skript Debian 8

--- scripts/prepare-odoo-server-debian8.sh
+++ scripts/prepare-odoo-server-debian8.sh
@@ -38,25 +38,92 @@ else
 fi
 
 echo "apt-get packages will be install.."
-apt-get install ghostscript graphviz antiword git libpq-dev poppler-utils \
- python-pip build-essential libfreetype6-dev curl python-magic libjpeg-dev
+apt-get install -y --no-install-recommends \
+			ghostscript  \
+			graphviz  \
+			antiword  \
+			poppler-utils \
+            curl \
+            build-essential \
+            libfreetype6-dev \
+            libjpeg-dev \
+            wget \
+            sqlite3 
 
 echo "apt-get python packages will be install.."
-apt-get install python-dateutil python-pypdf python-requests \
- python-feedparser python-gdata python-ldap python-libxslt1 \
- python-lxml python-mako python-openid python-psycopg2 \
- python-pybabel python-pychart python-pydot python-pyparsing \
- python-reportlab python-simplejson python-tz python-vatnumber \
- python-vobject python-webdav python-werkzeug python-xlwt \
- python-yaml python-zsi python-docutils python-psutil \
- python-unittest2 python-mock python-jinja2 python-dev \
- python-pdftools python-decorator python-openssl python-babel \
- python-imaging python-reportlab-accel \ python-passlib \
- python-paramiko python-software-properties python-matplotlib
+apt-get install -y --no-install-recommends \
+ 			python-pip  \
+ 			python-magic \            
+            python-dateutil \ 
+            python-pypdf \
+            python-requests \
+ 			python-feedparser \ 
+ 			python-gdata \ 
+ 			python-ldap \
+ 			python-libxslt1 \
+ 			python-lxml \ 
+ 			python-mako \ 
+ 			python-openid \
+ 			python-psycopg2 \
+ 			python-pybabel \ 
+ 			python-pychart \
+ 			python-pydot \
+ 			python-pyparsing \
+ 			python-reportlab \
+ 			python-simplejson \
+ 			python-tz \
+ 			python-vatnumber \
+ 			python-vobject \ 
+ 			python-webdav \
+ 			python-werkzeug \
+ 			python-xlwt \
+ 			python-yaml \
+ 			python-zsi \
+ 			python-docutils \ 
+ 			python-psutil \
+ 			python-unittest2 \ 
+ 			python-mock \ 
+ 			python-jinja2 \
+ 			python-dev \
+ 			python-pdftools \
+ 			python-decorator \ 
+ 			python-openssl \ 
+ 			python-babel \
+ 			python-imaging \ 
+ 			python-reportlab-accel \
+ 			python-paramiko \ 
+ 			python-software-properties \ 
+ 			python-matplotlib \
+            python-support \
+            python-passlib \
+            python-pyinotify \
+            python-gevent
 
 echo "pip packages will be install.."
-pip install passlib beautifulsoup4 evdev reportlab qrcode polib unidecode validate_email pyDNS pysftp python-slugify odoorpc
-easy_install pyinotify
+        pip install passlib \
+        && pip install beautifulsoup4 \ 
+        && pip install evdev \
+        && pip install reportlab \
+        && pip install qrcode \
+        && pip install polib \
+        && pip install unidecode \
+        && pip install validate_email \
+        && pip install pyDNS \ 
+        && pip install pysftp \
+        && pip install python-slugify \
+        && pip install six==1.4 \
+        && pip install paramiko==1.9.0 \
+        && pip install pycrypto==2.4 \
+        && pip install pyinotify \
+        && pip install ecdsa==0.11 \
+        && pip install sphinx \
+        && pip install babel==1.3 \
+        && pip install Pygments==2.0 \
+        && pip install docutils==0.11 \
+        && pip install markupsafe \
+        && pip install pytz \
+        && pip install Jinja2==2.3 \ 
+        && pip install odoorpc 
 
 echo "npm packages will be install.."
 curl -sL https://deb.nodesource.com/setup_0.12 | bash -
