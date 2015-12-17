eee50929af8768431b00b8616c659d758a2a34fe
Artur Bertram <a.bertram@equitania.de>
2015-12-17 13:43:22 +0100

    [ADD] Added compatibility modul for the equitania and stock_account module

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,4 +1,8 @@
 ### 17.12.2015
+### eq_stock_account_compatibility, 1.0.0
+#### Veröffentlichung
+- Kompatibilitätsmodul für das equitania und stock_account Modul, wodurch die Berechnung des Lagerwertes (Durchschnitt) funktioniert.
+- Aktuelle Bestandsbewertung des Lagers überarbeitet, sodass nicht stock.moves für die id genutzt werden, sondern die stock.quants. Somit fehlen keine Einträge, wenn in einem stock.move unterscheidliche stock.quants hinterlegt sind.
 ### equitania
 #### Bugfix, 1.0.103
 - Fehlerhafte compute function überarbeitet.
---
+++ eq_stock_account_compatibility/stock.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo Addon, Open Source Management Solution
+#    Copyright (C) 2014-now Equitania Software GmbH(<http://www.equitania.de>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+#Old API, Remove New API import if the Old API is used. Otherwise you'll get an import error.
+from datetime import datetime
+from openerp import tools
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+
+class stock_move(osv.osv):
+    _inherit = "stock.move"
+
+    def action_done(self, cr, uid, ids, context=None):
+        self.product_price_update_before_done(cr, uid, ids, context=context)
+        res = super(stock_move, self).action_done(cr, uid, ids, context=context)
+        self.product_price_update_after_done(cr, uid, ids, context=context)
+        return res
+
+
+
+class stock_history(osv.osv):
+    _inherit = 'stock.history'
+    
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, 'stock_history')
+        cr.execute("""
+            CREATE OR REPLACE VIEW stock_history AS (
+              SELECT MIN(id) as id,
+                move_id,
+                location_id,
+                company_id,
+                product_id,
+                product_categ_id,
+                SUM(quantity) as quantity,
+                date,
+                price_unit_on_quant,
+                source
+                FROM
+                ((SELECT
+                    quant.id AS id,
+                    stock_move.id AS move_id,
+                    dest_location.id AS location_id,
+                    dest_location.company_id AS company_id,
+                    stock_move.product_id AS product_id,
+                    product_template.categ_id AS product_categ_id,
+                    quant.qty AS quantity,
+                    stock_move.date AS date,
+                    quant.cost as price_unit_on_quant,
+                    stock_move.origin AS source
+                FROM
+                    stock_move
+                JOIN
+                    stock_quant_move_rel on stock_quant_move_rel.move_id = stock_move.id
+                JOIN
+                    stock_quant as quant on stock_quant_move_rel.quant_id = quant.id
+                JOIN
+                   stock_location dest_location ON stock_move.location_dest_id = dest_location.id
+                JOIN
+                    stock_location source_location ON stock_move.location_id = source_location.id
+                JOIN
+                    product_product ON product_product.id = stock_move.product_id
+                JOIN
+                    product_template ON product_template.id = product_product.product_tmpl_id
+                WHERE quant.qty>0 AND stock_move.state = 'done' AND dest_location.usage in ('internal', 'transit')
+                  AND (
+                    (source_location.company_id is null and dest_location.company_id is not null) or
+                    (source_location.company_id is not null and dest_location.company_id is null) or
+                    source_location.company_id != dest_location.company_id or
+                    source_location.usage not in ('internal', 'transit'))
+                ) UNION ALL
+                (SELECT
+                    (-1) * quant.id AS id,
+                    stock_move.id AS move_id,
+                    source_location.id AS location_id,
+                    source_location.company_id AS company_id,
+                    stock_move.product_id AS product_id,
+                    product_template.categ_id AS product_categ_id,
+                    - quant.qty AS quantity,
+                    stock_move.date AS date,
+                    quant.cost as price_unit_on_quant,
+                    stock_move.origin AS source
+                FROM
+                    stock_move
+                JOIN
+                    stock_quant_move_rel on stock_quant_move_rel.move_id = stock_move.id
+                JOIN
+                    stock_quant as quant on stock_quant_move_rel.quant_id = quant.id
+                JOIN
+                    stock_location source_location ON stock_move.location_id = source_location.id
+                JOIN
+                    stock_location dest_location ON stock_move.location_dest_id = dest_location.id
+                JOIN
+                    product_product ON product_product.id = stock_move.product_id
+                JOIN
+                    product_template ON product_template.id = product_product.product_tmpl_id
+                WHERE quant.qty>0 AND stock_move.state = 'done' AND source_location.usage in ('internal', 'transit')
+                 AND (
+                    (dest_location.company_id is null and source_location.company_id is not null) or
+                    (dest_location.company_id is not null and source_location.company_id is null) or
+                    dest_location.company_id != source_location.company_id or
+                    dest_location.usage not in ('internal', 'transit'))
+                ))
+                AS foo
+                GROUP BY move_id, location_id, company_id, product_id, product_categ_id, date, price_unit_on_quant, source
+            )""")
