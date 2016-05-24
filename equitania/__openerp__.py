12b86431a6aabd7589e6753f58dd013b07a753ce
Martin Schmid <m.schmid@equitania.com>
2016-05-24 10:58:57 +0200

    [CHG] Korrektur bei Korrekturrechnung/Gutschrift

--- equitania/__openerp__.py
+++ equitania/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Equitania Erweiterungsmodul',
     'license': 'AGPL-3',
-    'version': '1.1.37',
+    'version': '1.1.38',
     'description': """
         Equitania Software GmbH
     """,
