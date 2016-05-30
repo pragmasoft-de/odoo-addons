c0ee70ef426ca705d1a44af551350392a4eaa1e2
Robin Prijatel <r.prijatel@equitania.com>
2016-05-30 09:46:53 +0200

    [FIX] - Fehlermeldung bei dem Druck von Ausgangsrechnungen tritt nun nicht mehr auf. Die Bedingung für das Ersetzen des Wertes und des Datums wird nun nur ausgeführt, wenn ein Wert in "Date" (Zahlungsfrist: Platzhalter für das Datum) und "Value" (Zahlungsfrist: Platzhalter für den entsprechenden Wert) vorhanden ist.

--- equitania/__openerp__.py
+++ equitania/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Equitania Erweiterungsmodul',
     'license': 'AGPL-3',
-    'version': '1.1.50',
+    'version': '1.1.51',
     'description': """
         Equitania Software GmbH
     """,
